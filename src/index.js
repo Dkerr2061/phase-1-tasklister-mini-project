1c317883f222258fbb7d6e0c9ca9c59ff87f1d75
David Kerr <kerrky@gmail.com>
2024-02-02 14:26:49 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    newTaskList(e.target.new_task_description.value)
+    form.reset()
+  })
+  
 });
+
+function newTaskList(task) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', deleteTask)
+  btn.textContent = 'Delete'
+  p.textContent = `${task} `
+  p.appendChild(btn)
+  document.getElementById('tasks').appendChild(p)
+  
+}
+
+function deleteTask(e) {
+  e.target.parentNode.remove()
+}
+
+
